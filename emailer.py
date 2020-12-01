c687fc4a775ad35b8cbaf9cfc5c2c66ba0978869
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-12-01 15:26:21 +0000

    sends im.csv

---
+++ emailer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import os
+import smtplib,ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email import encoders
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def send_mail(send_from,send_to,subject,text,files,server,port,username='',password='',isTls=True):
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = send_to
+    msg['Date'] = formatdate(localtime = True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text))
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open("im.csv", "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="im.csv"')
+    msg.attach(part)
+
+    #context = ssl.SSLContext(ssl.PROTOCOL_SSLv3)
+    #SSL connection only working on Python 3+
+    smtp = smtplib.SMTP(server, port)
+    if isTls:
+        smtp.starttls()
+    smtp.login(username,password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+port = 587  # For SSL
+server = "smtp.gmail.com"
+send_from = os.getenv("send_from")  # Enter your address
+send_to = os.getenv("send_to")  # Enter receiver address
+password = os.getenv("password")
+subject = "test"
+text = "teXt"
+files = ""
+
+print(server,"\n", send_from,"\n", send_to)
+
+send_mail(send_from,send_to,subject,text,files,server,port,username=send_from,password=password,isTls=True)
